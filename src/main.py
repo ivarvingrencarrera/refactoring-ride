c9b76d49697093e14a851d0e72f3161efe1fbe1e
Ivar Carrera <ivar.carrera@outlook.com>
2023-05-03 13:41:22 -0300

    Code Smell: Changing dist variable name to distance

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def calculate_ride(segments):
     result = 0
     for segment in segments:
-        if segment.get("dist") is not None and segment.get("dist") is not None and isinstance(segment.get("dist"), int) and segment.get("dist") > 0:
+        if segment.get("distance") is not None and segment.get("distance") is not None and isinstance(segment.get("distance"), int) and segment.get("distance") > 0:
             if segment.get("ds") is not None and isinstance(segment.get("ds"), datetime) :
                 
                 # overnight
@@ -13,22 +13,22 @@ def calculate_ride(segments):
                     
                     # not sunday
                     if segment.get("ds").weekday() != 6:
-                        result += segment.get("dist") * 3.90
+                        result += segment.get("distance") * 3.90
                     
                     # sunday
                     else:
-                        result += segment.get("dist") * 5
+                        result += segment.get("distance") * 5
                 else:
                     # sunday
                     if segment.get("ds").weekday() == 6:
-                        result += segment.get("dist") * 2.9
+                        result += segment.get("distance") * 2.9
                     else:
-                        result += segment.get("dist") * 2.10
+                        result += segment.get("distance") * 2.10
             else:
                 # print(d)
                 return -2
         else:
-            # print(dist)
+            # print(distance)
             return -1
 
     if result < 10:
