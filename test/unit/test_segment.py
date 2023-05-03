9c058a5d8efa8456cedb80de2c60f8fb97b105e9
Ivar Carrera <ivar.carrera@outlook.com>
2023-05-03 20:27:20 -0300

    Create SegmentTest class

---
+++ test/unit/test_segment.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import unittest
+
+from src.segment import Segment
+
+
+class SegmentTest(unittest.TestCase):
+    def test_must_return_negative_if_distance_is_invalid(self) -> None:
+        with self.assertRaises(ValueError) as context:
+            Segment(-10.0, datetime.fromisoformat('2021-03-07T23:00:00'))
+        self.assertEqual(str(context.exception), 'Invalid distance')
+
+    def test_must_return_negative_if_date_is_invalid(self) -> None:
+        with self.assertRaises(ValueError) as context:
+            Segment(10.0, 'abc')   # type: ignore
+        self.assertEqual(str(context.exception), 'Invalid date')
