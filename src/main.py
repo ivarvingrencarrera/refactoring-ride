261bcde2773996da8844ac530ec4e77b61e03ef6
Ivar Carrera <ivar.carrera@outlook.com>
2023-05-03 13:42:29 -0300

    Code Smell: Changing ds variable name to date

--- src/main.py
+++ src/main.py
@@ -5,14 +5,14 @@ def calculate_ride(segments):
     result = 0
     for segment in segments:
         if segment.get("distance") is not None and segment.get("distance") is not None and isinstance(segment.get("distance"), int) and segment.get("distance") > 0:
-            if segment.get("ds") is not None and isinstance(segment.get("ds"), datetime) :
+            if segment.get("date") is not None and isinstance(segment.get("date"), datetime) :
                 
                 # overnight
                 
-                if segment.get("ds").hour >= 22 or segment.get("ds").hour <= 6:
+                if segment.get("date").hour >= 22 or segment.get("date").hour <= 6:
                     
                     # not sunday
-                    if segment.get("ds").weekday() != 6:
+                    if segment.get("date").weekday() != 6:
                         result += segment.get("distance") * 3.90
                     
                     # sunday
@@ -20,7 +20,7 @@ def calculate_ride(segments):
                         result += segment.get("distance") * 5
                 else:
                     # sunday
-                    if segment.get("ds").weekday() == 6:
+                    if segment.get("date").weekday() == 6:
                         result += segment.get("distance") * 2.9
                     else:
                         result += segment.get("distance") * 2.10
