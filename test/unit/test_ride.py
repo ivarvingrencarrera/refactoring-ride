0ae0cc226cb3c2a94495c4f7b87c20de3829d4e9
Ivar Carrera <ivar.carrera@outlook.com>
2023-05-03 20:26:02 -0300

    Create RideTest class

---
+++ test/unit/test_ride.py
@@ -0,0 +1,36 @@
+import unittest
+from datetime import datetime
+
+from src.ride import Ride
+
+
+class RideTest(unittest.TestCase):
+    def test_must_complete_a_ride_on_a_weekday_at_a_regular_hour(self) -> None:
+        ride = Ride()
+        ride.add_segment(10.0, datetime.fromisoformat('2021-03-10T10:00:00'))
+        fare = ride.calculate_fare()
+        self.assertEqual(fare, 21.0)
+
+    def test_must_complete_a_ride_on_a_weekday_at_a_overnight_hour(self) -> None:
+        ride = Ride()
+        ride.add_segment(10.0, datetime.fromisoformat("2021-03-10T23:00:00"))
+        fare = ride.calculate_fare()
+        self.assertEqual(fare, 39.0)
+
+    def test_must_complete_a_ride_on_a_sunday_at_a_regular_hour(self) -> None:
+        ride = Ride()
+        ride.add_segment(10.0, datetime.fromisoformat('2021-03-07T10:00:00'))
+        fare = ride.calculate_fare()
+        self.assertEqual(fare, 29.0)
+
+    def test_must_complete_a_ride_on_a_sunday_at_a_overnight_hour(self) -> None:
+        ride = Ride()
+        ride.add_segment(10.0, datetime.fromisoformat('2021-03-07T23:00:00'))
+        fare = ride.calculate_fare()
+        self.assertEqual(fare, 50.0)
+
+    def test_must_return_minimum_fare(self) -> None:
+        ride = Ride()
+        ride.add_segment(1.0, datetime.fromisoformat('2021-03-10T10:00:00'))
+        fare = ride.calculate_fare()
+        self.assertEqual(fare, 10.0)
