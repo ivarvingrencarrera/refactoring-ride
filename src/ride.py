a1e0c0e634c0fd7c073e64b2d39430b785b0140f
Ivar Carrera <ivar.carrera@outlook.com>
2023-05-03 19:52:12 -0300

    Create Ride class

---
+++ src/ride.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from src.segment import Segment
+
+
+class Ride:
+    SUNDAY_OVERNIGHT_FARE = 5
+    OVERNIGHT_FARE = 3.9
+    SUNDAY_FARE = 2.9
+    NORMAL_FARE = 2.1
+    MIN_FARE = 10
+
+    def __init__(self) -> None:
+        self.segments: list[Segment] = []
+
+    def add_segment(self, distance: float, date: datetime) -> None:
+        self.segments.append(Segment(distance, date))
+
+    def calculate_fare(self) -> float:
+        fare: float = 0
+        for segment in self.segments:
+            if segment.is_overnight() and segment.is_sunday():
+                fare += segment.distance * self.SUNDAY_OVERNIGHT_FARE
+            elif segment.is_overnight() and not segment.is_sunday():
+                fare += segment.distance * self.OVERNIGHT_FARE
+            elif segment.is_sunday() and segment.is_sunday():
+                fare += segment.distance * self.SUNDAY_FARE
+            elif segment.is_sunday() and not segment.is_sunday():
+                fare += segment.distance * self.NORMAL_FARE
+        return 10 if fare <= self.MIN_FARE else fare
